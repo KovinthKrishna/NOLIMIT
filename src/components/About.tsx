e7aa8f2071cad11adad7bcc28d6d6a27afbaaff0
KovinthKrishna <KovinthKrishna@gmail.com>
2024-12-12 20:25:20 +0530

    Change to Spring Boot

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,56 +1,56 @@
 import { Button, HStack, Image, Input, Text, VStack } from "@chakra-ui/react";
-import logo from "../assets/logo.svg";
-import { useContext, useEffect, useState } from "react";
 import axios from "axios";
+import { useContext, useEffect, useState } from "react";
 import { AppContext } from "../App";
+import logo from "../assets/logo.svg";
 import { serverUrl } from "../hooks/useItem";
 
 const About = () => {
-    const handleShowAlert = useContext(AppContext);
-    const [email, setEmail] = useState("");
-    const [button, setButton] = useState(false);
-    useEffect(() => {
-        setButton(email.includes("@"));
-    }, [email]);
+  const handleShowAlert = useContext(AppContext);
+  const [email, setEmail] = useState("");
+  const [button, setButton] = useState(false);
+  useEffect(() => {
+    setButton(email.includes("@"));
+  }, [email]);
 
-    const submit = async () => {
-        try {
-            const result = await axios.post(`${serverUrl}/add/users`, {
-                email,
-            });
-            handleShowAlert(result.data);
-            setEmail("");
-        } catch (err) {
-            console.error(err);
-        }
-    };
+  const submit = async () => {
+    try {
+      const result = await axios.post(`${serverUrl}/users`, {
+        email,
+      });
+      handleShowAlert(result.data);
+      setEmail("");
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
-    return (
-        <VStack alignItems="start">
-            <Image src={logo} boxSize="120px" maxHeight="75px"></Image>
-            <Text fontSize="12px" fontWeight="bolder" lineHeight="30px">
-                NOLIMIT, retail chain takes pride in its Sri Lankan roots but
-                offers a world-class retail experience that drives customer
-                loyalty while simultaneously setting new benchmarks in
-                customer-centricity to take the art of retailing to a new level.
-            </Text>
-            <HStack paddingY="30px">
-                <Input
-                    placeholder="Your email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                ></Input>
-                <Button
-                    colorScheme="teal"
-                    variant="solid"
-                    isDisabled={!button}
-                    onClick={submit}
-                >
-                    Subscribe
-                </Button>
-            </HStack>
-        </VStack>
-    );
+  return (
+    <VStack alignItems="start">
+      <Image src={logo} boxSize="120px" maxHeight="75px"></Image>
+      <Text fontSize="12px" fontWeight="bolder" lineHeight="30px">
+        NOLIMIT, retail chain takes pride in its Sri Lankan roots but offers a
+        world-class retail experience that drives customer loyalty while
+        simultaneously setting new benchmarks in customer-centricity to take the
+        art of retailing to a new level.
+      </Text>
+      <HStack paddingY="30px">
+        <Input
+          placeholder="Your email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        ></Input>
+        <Button
+          colorScheme="teal"
+          variant="solid"
+          isDisabled={!button}
+          onClick={submit}
+        >
+          Subscribe
+        </Button>
+      </HStack>
+    </VStack>
+  );
 };
 
 export default About;
--- src/hooks/useItem.ts
+++ src/hooks/useItem.ts
@@ -1,54 +1,49 @@
 import axios from "axios";
 
 export interface Item {
-    _id: string;
-    id: number;
-    count: number;
+  id: number;
+  count: number;
 }
 
-export const serverUrl = import.meta.env.VITE_URL ?? "http://localhost:3000";
+export const serverUrl = import.meta.env.VITE_URL ?? "http://localhost:8080";
 
 export const fetchItem = async () => {
-    try {
-        const result = await axios.get(`${serverUrl}/fetch/items`);
-        return result.data;
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const result = await axios.get(`${serverUrl}/items`);
+    return result.data;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
 export const newItem = async (id: number, count: number) => {
-    try {
-        const result = await axios.post(`${serverUrl}/add/items`, {
-            id: id,
-            count: count,
-        });
-        return result.data;
-    } catch (err) {
-        console.error(err);
-    }
+  try {
+    const result = await axios.post(`${serverUrl}/items`, {
+      id: id,
+      count: count,
+    });
+    return result.data;
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-export const updateItem = async (
-    item: { _id: string; count: number },
-    change: number
-) => {
-    try {
-        await axios.put(`${serverUrl}/update/items/` + item._id, {
-            count: item.count + change,
-        });
-    } catch (err) {
-        console.error(err);
-    }
+export const updateItem = async (item: Item, change: number) => {
+  try {
+    await axios.put(`${serverUrl}/items`, {
+      id: item.id,
+      count: item.count + change,
+    });
+  } catch (err) {
+    console.error(err);
+  }
 };
 
-export const deleteItem = async (item: { _id: string }) => {
-    try {
-        const result = await axios.delete(
-            `${serverUrl}/delete/items/` + item._id
-        );
-        return result.data;
-    } catch (err) {
-        console.error(err);
-    }
+export const deleteItem = async (item: Item) => {
+  try {
+    const result = await axios.delete(`${serverUrl}/items/` + item.id);
+    return result.data;
+  } catch (err) {
+    console.error(err);
+  }
 };
